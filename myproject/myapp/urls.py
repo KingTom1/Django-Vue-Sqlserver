6cb44d7ba98473f57e7b46c57a901638f629732e
KingTom1 <38772091+KingTom1@users.noreply.github.com>
2019-03-21 11:08:25 +0800

    1321

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url, include
+import myapp.views
+
+urlpatterns = [
+    url(r'add_book$', myapp.views.add_book, ),
+    url(r'show_books$', myapp.views.show_books, ),
+    ]
